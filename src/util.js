55e0b5f4b6bafe34b2249e0d6bcd96bf21a0bdeb
newt0 <py_lambda@yahoo.co.jp>
2020-09-30 15:24:48 +0900

    fix: make sort function more beautiful

--- src/util.js
+++ src/util.js
@@ -1,13 +1,5 @@
 export const sortData = (data) => {
   const sortedData = [...data]; // copy data
 
-  sortedData.sort((a, b) => {
-    if (a.cases > b.cases) {
-      return -1; // -1 stands for "false"
-    } else {
-      return 1; // 1 stands for "true"
-    }
-  });
-
-  return sortedData;
+  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1) )
 };
